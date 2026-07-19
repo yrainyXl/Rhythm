eb6759d89ebe6222a1d6d16b3e4e9fcc95273c96
xulei <xulei@7moor.com>
2026-07-19 21:54:17 +0800

    feat(records): route /records/reflection/today

---
+++ src/app/records/reflection/today/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { ReflectionDetailPage } from '@/features/records/components/reflection-detail-page'
+
+export default function ReflectionTodayPage() {
+  return <ReflectionDetailPage />
+}
